65ed75809e19b3a05d7bed445ae35d225e608926
a_remix_user <maxnadeau@rdwrs.com>
2023-01-27 09:31:28 +0000

    Add attempt at GPT2 Clone

--- train_gpt.py
+++ train_gpt.py
@@ -1,24 +1,43 @@
 # %%
-from transformers import GPT2Tokenizer, GPT2Model
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch as t
 
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-model = GPT2Model.from_pretrained("gpt2")
-text = "Replace me by any text you'd like."
-eos_token = "<|endoftext|>"
-prompt = text
-print("Starting prompt: " + prompt)
-# %%
-for i in range(100):
-    encoded_input = tokenizer(prompt, return_tensors="pt")
-    output = model(**encoded_input)
-    mlp_output = output.last_hidden_state[0, -1, :]
-    cos_similarity = t.nn.functional.cosine_similarity(mlp_output, model.wte.weight, dim=1)
-    next_token = cos_similarity.argmax()
-    next_string = tokenizer.decode(next_token)
-    if next_string == eos_token:
-        break
-    prompt = prompt + next_string
-    print("Current generation: " + prompt)
+def test_model():
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    model = GPT2LMHeadModel.from_pretrained("gpt2")
+    text = "Replace me by any text you'd like."
+    eos_token = "<|endoftext|>"
+    prompt = text
+    print("Starting prompt: " + prompt)
+
+    for i in range(100):
+        encoded_input = tokenizer(prompt, return_tensors="pt")
+        next_token = model(**encoded_input).logits[0, -1].argmax()
+        next_string = tokenizer.decode(next_token)
+        if next_string == eos_token:
+            break
+        prompt = prompt + next_string
+        print("Current generation: " + prompt)
 
 # %%
+
+class GPT2Block(t.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        
+
+    def forward(self, x):
+        pass
+
+class GPT2Clone(t.nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.wte = t.nn.Embedding(50257, 768)
+        self.wpe = t.nn.Embedding(1024, 768)
+        self.drop = t.nn.Dropout(0.1)
+    
+    def forward(self, x):
+
+    
+
